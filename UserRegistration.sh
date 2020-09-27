f4b2a0add566f77b189d6a16d5631366ea525e19
7798209972 <patlesachin1@gmail.com>
2020-09-27 16:12:28 +0530

    First Name Validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,3 +6,18 @@ echo "Welcome to User Registration Problem"
 echo "====================================";
 printf "\n";
 
+#Getting First Name
+
+read -p "Enter First Name : " first_name;
+
+#Regex for cheking First Name
+regex_name="^[A-Z]{1}[a-z]{2,}$";
+
+#Cheking First Name
+
+if [[ $first_name =~ $regex_name ]]
+then
+	echo "Success";
+else
+	echo "Error..! First name should be start with Cap and has minimum 3 characters";
+fi
